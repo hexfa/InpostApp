4f2607a3a509323dbc38b73be95634926ea38318
@hexfa <amir.dehdarian2009@gmail.com>
2023-05-23 17:58:13 +0330

    Add ShipmentItemAdapter.kt and dependencies.

--- app/src/main/java/pl/inpost/recruitmenttask/view/activity/HomeActivity.kt
+++ app/src/main/java/pl/inpost/recruitmenttask/view/activity/HomeActivity.kt
@@ -2,20 +2,34 @@ package pl.inpost.recruitmenttask.view.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
 import pl.inpost.recruitmenttask.R
-import pl.inpost.recruitmenttask.view.fragment.ShipmentListFragment
+import pl.inpost.recruitmenttask.databinding.ActivityHomeBinding
 
 @AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
 
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-        if (savedInstanceState == null) {
+        // setContentView(R.layout.activity_home)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+        bottomNavigation()
+
+        /*if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, ShipmentListFragment.newInstance())
                 .commitNow()
-        }
+        }*/
     }
+
+    private fun bottomNavigation(){
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController    }
 }
--- app/src/main/java/pl/inpost/recruitmenttask/view/fragment/ShipmentListFragment.kt
+++ app/src/main/java/pl/inpost/recruitmenttask/view/fragment/ShipmentListFragment.kt
@@ -7,7 +7,6 @@ import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import pl.inpost.recruitmenttask.R
 import pl.inpost.recruitmenttask.databinding.FragmentShipmentListBinding
-import pl.inpost.recruitmenttask.databinding.ShipmentItemBinding
 import pl.inpost.recruitmenttask.viewmodel.ShipmentListViewModel
 
 @AndroidEntryPoint
@@ -34,13 +33,7 @@ class ShipmentListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.viewState.observe(requireActivity()) { shipments ->
-            shipments.forEach { shipmentNetwork ->
-                val shipmentItemBinding = ShipmentItemBinding.inflate(layoutInflater).apply {
-                    shipmentNumber.text = shipmentNetwork.number
-                    status.text = shipmentNetwork.status
-                }
-                binding?.scrollViewContent?.addView(shipmentItemBinding.root)
-            }
+
         }
     }
 
