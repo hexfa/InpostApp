3353f53070762da25280f41e48e7da5292800f00
@hexfa <amir.dehdarian2009@gmail.com>
2023-05-24 09:04:12 +0430

    Sort list items in group by pickupDate,expireDate,storedDate,number.

--- app/build/generated/source/kapt/debug/pl/inpost/recruitmenttask/databinding/FragmentShipmentListBindingImpl.java
+++ app/build/generated/source/kapt/debug/pl/inpost/recruitmenttask/databinding/FragmentShipmentListBindingImpl.java
@@ -15,27 +15,33 @@ public class FragmentShipmentListBindingImpl extends FragmentShipmentListBinding
         sIncludes = null;
         sViewsWithIds = new android.util.SparseIntArray();
         sViewsWithIds.put(R.id.header, 1);
-        sViewsWithIds.put(R.id.text, 2);
-        sViewsWithIds.put(R.id.layout_in_transit, 3);
-        sViewsWithIds.put(R.id.divider1_left, 4);
-        sViewsWithIds.put(R.id.divider_tex_in_transit, 5);
-        sViewsWithIds.put(R.id.divider1_right, 6);
-        sViewsWithIds.put(R.id.recycler_shipment_in_transit, 7);
-        sViewsWithIds.put(R.id.layout_pickup, 8);
-        sViewsWithIds.put(R.id.divider2_left, 9);
-        sViewsWithIds.put(R.id.divider_tex_pickup, 10);
-        sViewsWithIds.put(R.id.divider2_right, 11);
-        sViewsWithIds.put(R.id.recycler_shipment_pickup, 12);
-        sViewsWithIds.put(R.id.layout_in_progress, 13);
-        sViewsWithIds.put(R.id.divider3_left, 14);
-        sViewsWithIds.put(R.id.divider_tex_in_progress, 15);
-        sViewsWithIds.put(R.id.divider3_right, 16);
-        sViewsWithIds.put(R.id.recycler_shipment_in_progress, 17);
-        sViewsWithIds.put(R.id.layout_pending, 18);
-        sViewsWithIds.put(R.id.divider4_left, 19);
-        sViewsWithIds.put(R.id.divider_tex_pending, 20);
-        sViewsWithIds.put(R.id.divider4_right, 21);
-        sViewsWithIds.put(R.id.recycler_shipment_pending, 22);
+        sViewsWithIds.put(R.id.menu_button, 2);
+        sViewsWithIds.put(R.id.text, 3);
+        sViewsWithIds.put(R.id.layout_in_transit, 4);
+        sViewsWithIds.put(R.id.divider1_left, 5);
+        sViewsWithIds.put(R.id.divider_tex_in_transit, 6);
+        sViewsWithIds.put(R.id.divider1_right, 7);
+        sViewsWithIds.put(R.id.recycler_shipment_in_transit, 8);
+        sViewsWithIds.put(R.id.layout_pickup, 9);
+        sViewsWithIds.put(R.id.divider2_left, 10);
+        sViewsWithIds.put(R.id.divider_tex_pickup, 11);
+        sViewsWithIds.put(R.id.divider2_right, 12);
+        sViewsWithIds.put(R.id.recycler_shipment_pickup, 13);
+        sViewsWithIds.put(R.id.layout_in_progress, 14);
+        sViewsWithIds.put(R.id.divider3_left, 15);
+        sViewsWithIds.put(R.id.divider_tex_in_progress, 16);
+        sViewsWithIds.put(R.id.divider3_right, 17);
+        sViewsWithIds.put(R.id.recycler_shipment_in_progress, 18);
+        sViewsWithIds.put(R.id.layout_pending, 19);
+        sViewsWithIds.put(R.id.divider4_left, 20);
+        sViewsWithIds.put(R.id.divider_tex_pending, 21);
+        sViewsWithIds.put(R.id.divider4_right, 22);
+        sViewsWithIds.put(R.id.recycler_shipment_pending, 23);
+        sViewsWithIds.put(R.id.layout_filter, 24);
+        sViewsWithIds.put(R.id.divider_filter_left, 25);
+        sViewsWithIds.put(R.id.divider_text_another, 26);
+        sViewsWithIds.put(R.id.divider_filter_right, 27);
+        sViewsWithIds.put(R.id.recycler_filter, 28);
     }
     // views
     @NonNull
@@ -46,32 +52,38 @@ public class FragmentShipmentListBindingImpl extends FragmentShipmentListBinding
     // Inverse Binding Event Handlers
 
     public FragmentShipmentListBindingImpl(@Nullable androidx.databinding.DataBindingComponent bindingComponent, @NonNull View root) {
-        this(bindingComponent, root, mapBindings(bindingComponent, root, 23, sIncludes, sViewsWithIds));
+        this(bindingComponent, root, mapBindings(bindingComponent, root, 29, sIncludes, sViewsWithIds));
     }
     private FragmentShipmentListBindingImpl(androidx.databinding.DataBindingComponent bindingComponent, View root, Object[] bindings) {
         super(bindingComponent, root, 0
-            , (android.view.View) bindings[4]
-            , (android.view.View) bindings[6]
-            , (android.view.View) bindings[9]
-            , (android.view.View) bindings[11]
-            , (android.view.View) bindings[14]
-            , (android.view.View) bindings[16]
-            , (android.view.View) bindings[19]
-            , (android.view.View) bindings[21]
-            , (android.widget.TextView) bindings[15]
-            , (android.widget.TextView) bindings[5]
-            , (android.widget.TextView) bindings[20]
-            , (android.widget.TextView) bindings[10]
+            , (android.view.View) bindings[5]
+            , (android.view.View) bindings[7]
+            , (android.view.View) bindings[10]
+            , (android.view.View) bindings[12]
+            , (android.view.View) bindings[15]
+            , (android.view.View) bindings[17]
+            , (android.view.View) bindings[20]
+            , (android.view.View) bindings[22]
+            , (android.view.View) bindings[25]
+            , (android.view.View) bindings[27]
+            , (android.widget.TextView) bindings[16]
+            , (android.widget.TextView) bindings[6]
+            , (android.widget.TextView) bindings[21]
+            , (android.widget.TextView) bindings[11]
+            , (android.widget.TextView) bindings[26]
             , (android.widget.RelativeLayout) bindings[1]
-            , (androidx.constraintlayout.widget.ConstraintLayout) bindings[13]
-            , (androidx.constraintlayout.widget.ConstraintLayout) bindings[3]
-            , (androidx.constraintlayout.widget.ConstraintLayout) bindings[18]
-            , (androidx.constraintlayout.widget.ConstraintLayout) bindings[8]
-            , (androidx.recyclerview.widget.RecyclerView) bindings[17]
-            , (androidx.recyclerview.widget.RecyclerView) bindings[7]
-            , (androidx.recyclerview.widget.RecyclerView) bindings[22]
-            , (androidx.recyclerview.widget.RecyclerView) bindings[12]
-            , (android.widget.TextView) bindings[2]
+            , (androidx.constraintlayout.widget.ConstraintLayout) bindings[24]
+            , (androidx.constraintlayout.widget.ConstraintLayout) bindings[14]
+            , (androidx.constraintlayout.widget.ConstraintLayout) bindings[4]
+            , (androidx.constraintlayout.widget.ConstraintLayout) bindings[19]
+            , (androidx.constraintlayout.widget.ConstraintLayout) bindings[9]
+            , (android.widget.ImageButton) bindings[2]
+            , (androidx.recyclerview.widget.RecyclerView) bindings[28]
+            , (androidx.recyclerview.widget.RecyclerView) bindings[18]
+            , (androidx.recyclerview.widget.RecyclerView) bindings[8]
+            , (androidx.recyclerview.widget.RecyclerView) bindings[23]
+            , (androidx.recyclerview.widget.RecyclerView) bindings[13]
+            , (android.widget.TextView) bindings[3]
             );
         this.mboundView0 = (androidx.constraintlayout.widget.ConstraintLayout) bindings[0];
         this.mboundView0.setTag(null);
--- app/src/main/java/pl/inpost/recruitmenttask/view/fragment/ShipmentListFragment.kt
+++ app/src/main/java/pl/inpost/recruitmenttask/view/fragment/ShipmentListFragment.kt
@@ -5,14 +5,19 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.PopupMenu
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
+import pl.inpost.recruitmenttask.R
 import pl.inpost.recruitmenttask.databinding.FragmentShipmentListBinding
 import pl.inpost.recruitmenttask.model.local.ShipmentNetwork
 import pl.inpost.recruitmenttask.view.adapter.ShipmentItemAdapter
 import pl.inpost.recruitmenttask.viewmodel.ShipmentListViewModel
+
+private const val TAG = "ShipmentListFragment"
 
 @AndroidEntryPoint
 class ShipmentListFragment : Fragment() {
@@ -23,11 +28,14 @@ class ShipmentListFragment : Fragment() {
     private var shipmentItemAdapterPickup: ShipmentItemAdapter? = null
     private var shipmentItemAdapterInProcess: ShipmentItemAdapter? = null
     private var shipmentItemAdapterPending: ShipmentItemAdapter? = null
+    private var shipmentFilterItemAdapter: ShipmentItemAdapter? = null
+
 
     private var shipmentItemsInTransit = mutableListOf<ShipmentNetwork>()
     private var shipmentItemsPickup = mutableListOf<ShipmentNetwork>()
     private var shipmentItemInProgress = mutableListOf<ShipmentNetwork>()
     private var shipmentItemPending = mutableListOf<ShipmentNetwork>()
+    private var shipmentList = mutableListOf<ShipmentNetwork>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,8 +43,8 @@ class ShipmentListFragment : Fragment() {
         shipmentItemAdapterPickup = ShipmentItemAdapter()
         shipmentItemAdapterInProcess = ShipmentItemAdapter()
         shipmentItemAdapterPending = ShipmentItemAdapter()
+        shipmentFilterItemAdapter = ShipmentItemAdapter()
 
-        //setHasOptionsMenu(true)
     }
 
 
@@ -51,34 +59,104 @@ class ShipmentListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val menuButton: ImageButton = view.findViewById(R.id.menu_button)
+
+        // Set the click listener for the menu button
+        menuButton.setOnClickListener {
+            Log.d(TAG, "onViewCreated: " + "clicked")
+            showPopupMenu(menuButton)
+        }
+
+
+
+
         Log.d("ShipmentListFragment", "onViewCreated: " + "shipments")
 
         viewModel.getShipment().observe(requireActivity()) { shipments ->
+            shipmentList.addAll(shipments.shipments)
             //ToDo list should be fill here *****************************************
             // *****************************************************************************************************
             shipments.shipments.forEach {
-                when(it.status){
-                    "OUT_FOR_DELIVERY"->shipmentItemsInTransit.add(it)
-                    "RETURNED_TO_SENDER"->shipmentItemsInTransit.add(it)
-                    "SENT_FROM_SOURCE_BRANCH"->shipmentItemsInTransit.add(it)
-                    "READY_TO_PICKUP"->shipmentItemsPickup.add(it)
-                    "DELIVERED"->shipmentItemsPickup.add(it)
-                    "CONFIRMED"->shipmentItemInProgress.add(it)
-                    "ADOPTED_AT_SOURCE_BRANCH"->shipmentItemInProgress.add(it)
-                    "CREATED"->shipmentItemInProgress.add(it)
-                    "SENT_FROM_SOURCE_BRANCH"->shipmentItemInProgress.add(it)
-                    "AVIZO"->shipmentItemPending.add(it)
-                    "NOT_READY"->shipmentItemPending.add(it)
+                when (it.status) {
+                    "OUT_FOR_DELIVERY" -> shipmentItemsInTransit.add(it)
+                    "RETURNED_TO_SENDER" -> shipmentItemsInTransit.add(it)
+                    "SENT_FROM_SOURCE_BRANCH" -> shipmentItemsInTransit.add(it)
+                    "READY_TO_PICKUP" -> shipmentItemsPickup.add(it)
+                    "DELIVERED" -> shipmentItemsPickup.add(it)
+                    "CONFIRMED" -> shipmentItemInProgress.add(it)
+                    "ADOPTED_AT_SOURCE_BRANCH" -> shipmentItemInProgress.add(it)
+                    "CREATED" -> shipmentItemInProgress.add(it)
+                    "SENT_FROM_SOURCE_BRANCH" -> shipmentItemInProgress.add(it)
+                    "AVIZO" -> shipmentItemPending.add(it)
+                    "NOT_READY" -> shipmentItemPending.add(it)
 
-                    else->shipmentItemPending.add(it)
+                    else -> shipmentItemPending.add(it)
 
                 }
             }
 
 
-            setUpAdapter(shipmentItemsInTransit,shipmentItemsPickup,shipmentItemInProgress,shipmentItemPending)
+            setUpAdapter(
+                shipmentItemsInTransit,
+                shipmentItemsPickup,
+                shipmentItemInProgress,
+                shipmentItemPending
+            )
         }
     }
+
+    private fun showPopupMenu(anchorView: View) {
+        val popupMenu = PopupMenu(requireContext(), anchorView)
+        popupMenu.menuInflater.inflate(R.menu.shipment_list_menu, popupMenu.menu)
+
+        popupMenu.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.menu_expire_date -> {
+                    Log.d(TAG, "showPopupMenu: $shipmentList")
+                    showExpiryDateItem(shipmentList)
+                    true
+                    // Handle "Expire Date" menu item click
+                }
+
+                R.id.menu_pickup_date -> {
+                    // Handle "Pickup Date" menu item click
+                    showPickUpDateItem(shipmentList)
+
+                    true
+
+                }
+
+                R.id.menu_sorted_item -> {
+                    // Handle "Sorted Item" menu item click
+                    showStoredDateItem(shipmentList)
+                    true
+
+
+                }
+
+                R.id.menu_number -> {
+                    // Handle "Number" menu item click
+                    showNumberItem(shipmentList)
+                    true
+
+
+                }
+
+                R.id.menu_default -> {
+                    // Handle "Number" menu item click
+                    hideFilterItem()
+
+                    true
+                }
+                // Add more menu item cases as needed
+
+                else -> false
+            }
+        }
+
+        popupMenu.show()
+    }
+
 
     private fun setUpAdapter(
         shipmentItemsInTransit: List<ShipmentNetwork>?,
@@ -144,7 +222,83 @@ class ShipmentListFragment : Fragment() {
         binding = null
     }
 
+    private fun showExpiryDateItem(shipments: List<ShipmentNetwork>) {
+        val list = mutableListOf<ShipmentNetwork>()
+        for (shipment in shipments) {
+            if (shipment.expiryDate?.equals("") == false) {
+                Log.d(TAG, "showExpiryDateItem: $shipment")
+                list.add(shipment)
+            }
+        }
+        showFilterItem(list)
+
+    }
+
+    private fun showPickUpDateItem(shipments: List<ShipmentNetwork>) {
+        val list = mutableListOf<ShipmentNetwork>()
+        for (shipment in shipments) {
+            if (shipment.pickUpDate?.equals("") == false) {
+                list.add(shipment)
+            }
+        }
+        showFilterItem(list)
+
+    }
+
+    private fun showStoredDateItem(shipments: List<ShipmentNetwork>) {
+        val list = mutableListOf<ShipmentNetwork>()
+        for (shipment in shipments) {
+            if (shipment.storedDate?.equals("") == false) {
+                list.add(shipment)
+            }
+        }
+        showFilterItem(list)
+
+    }
+
+    private fun showNumberItem(shipments: List<ShipmentNetwork>) {
+        val list = mutableListOf<ShipmentNetwork>()
+        for (shipment in shipments) {
+            if (shipment.number?.equals("") == false) {
+                list.add(shipment)
+            }
+        }
+        showFilterItem(list)
+
+    }
+
+    private fun showFilterItem(shipment: List<ShipmentNetwork>?) {
+        binding?.layoutInProgress?.visibility = View.GONE
+        binding?.layoutPending?.visibility = View.GONE
+        binding?.layoutPickup?.visibility = View.GONE
+        binding?.layoutInTransit?.visibility = View.GONE
+        binding?.layoutFilter?.visibility = View.VISIBLE
+        Log.d(TAG, "showFilterItem: $shipment")
+        if (shipment != null) {
+            shipmentFilterItemAdapter?.addData(shipment)
+        }
+        Log.d(TAG, "showFilterItem: $shipmentFilterItemAdapter")
+        binding?.recyclerFilter?.adapter = shipmentFilterItemAdapter
+        binding?.recyclerFilter?.isNestedScrollingEnabled = false
+
+        binding?.recyclerFilter?.layoutManager = LinearLayoutManager(
+            requireContext(),
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+    }
+
+    private fun hideFilterItem() {
+        binding?.layoutInProgress?.visibility = View.VISIBLE
+        binding?.layoutPending?.visibility = View.VISIBLE
+        binding?.layoutPickup?.visibility = View.VISIBLE
+        binding?.layoutInTransit?.visibility = View.VISIBLE
+        binding?.layoutFilter?.visibility = View.GONE
+    }
+
+
     companion object {
         fun newInstance() = ShipmentListFragment()
     }
+
 }
